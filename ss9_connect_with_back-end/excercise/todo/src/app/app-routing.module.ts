1faddc36407c54c9e866bf66c8de850953721e06
ducvu1990 <you@example.com>
2023-03-14 22:08:28 +0700

    bai tap

---
+++ ss9_connect_with_back-end/excercise/todo/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {TodoEditComponent} from './todo-edit/todo-edit.component';
+
+
+const routes: Routes = [
+  {path: 'todos/edit/:id', component: TodoEditComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
